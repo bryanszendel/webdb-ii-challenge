b6a38de19be8f8911210afee95e6fd88618d84e4
Bryan Szendel <bryan.szendel@gmail.com>
2019-09-10 14:16:41 -0600

    GET request to /cars retrieves car data

--- api/server.js
+++ api/server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const helmet = require('helmet');
 
-// const carsRouter = require('')
-
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 
+const db = require('../data/db-config.js')
 
+server.get('/cars', (req, res) => {
+  db('cars')
+    .then(cars => {
+      res.status(200).json(cars)
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Failed to retrieve cars.'})
+    })
+})
 
 module.exports = server;
